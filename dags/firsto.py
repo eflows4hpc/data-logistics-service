043d093042d1502a250d22373323f78578a92c12
jrybicki-jsc <j.rybicki@fz-juelich.de>
2021-08-20 11:45:01 +0200

    simple dag for tests

---
+++ dags/firsto.py
@@ -0,0 +1,25 @@
+from datetime import timedelta
+
+from airflow import DAG
+
+from airflow.utils.dates import days_ago
+
+from airflow.operators.bash import BashOperator
+
+
+def_args = {
+     'owner': 'airflow',
+     'depends_on_past': False,
+     'email_on_failure': False,
+     'email_on_retry': False,
+     'retries': 1,
+     'retry_delay': timedelta(minutes=5)
+     
+        }
+
+with DAG('firsto', default_args=def_args, description='first dag', schedule_interval=timedelta(days=1), start_date=days_ago(2)) as dag:
+    t1 = BashOperator(task_id='print_date', bash_command='date')
+    t2 = BashOperator(task_id='do_noting', bash_command='sleep 5')
+
+    t1 >> t2
+
