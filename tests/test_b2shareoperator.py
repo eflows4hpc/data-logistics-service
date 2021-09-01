d7a73b26a1d8b1fa97a23f1c1b57005e5c087583
jrybicki-jsc <j.rybicki@fz-juelich.de>
2021-09-01 11:35:41 +0200

    adding test stub

---
+++ tests/test_b2shareoperator.py
@@ -0,0 +1,22 @@
+import unittest
+from airflow.utils.state import State
+from dags.b2shareoperator import B2ShareOperator
+from airflow import DAG
+from airflow.models.taskinstance import TaskInstance
+
+DEFAULT_DATE = '2019-10-03'
+TEST_DAG_ID = 'test_my_custom_operator'
+
+class B2ShareOperatorTest(unittest.TestCase):
+   def setUp(self):
+       self.dag = DAG(TEST_DAG_ID, schedule_interval='@daily', default_args={'start_date' : DEFAULT_DATE})
+       self.op = B2ShareOperator(
+           dag=self.dag,
+           task_id='test',
+       )
+       self.ti = TaskInstance(task=self.op, execution_date=DEFAULT_DATE)
+
+   def test_execute_no_trigger(self):
+       self.ti.run(ignore_ti_state=True)
+       assert self.ti.state == State.SUCCESS
+       # Assert something related to tasks results
---
+++ tests/test_dag.py
@@ -0,0 +1,13 @@
+from airflow.models import DagBag
+import unittest
+
+class TestADag(unittest.TestCase):
+   @classmethod
+   def setUpClass(cls):
+       cls.dagbag = DagBag()
+
+   def test_dag_loaded(self):
+       dag = self.dagbag.get_dag(dag_id='firsto')
+       assert self.dagbag.import_errors == {}
+       assert dag is not None
+       assert len(dag.tasks) == 1
