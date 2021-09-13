fdf67c826f9a05a8bc7713eef905e7aaacd57b6c
jrybicki-jsc <j.rybicki@fz-juelich.de>
2021-09-13 09:50:30 +0200

    params in dag

--- tests/test_b2shareoperator.py
+++ tests/test_b2shareoperator.py
@@ -4,13 +4,15 @@ from airflow.utils.dates import days_ago
 from dags.b2shareoperator import B2ShareOperator
 from airflow import DAG
 from airflow.models.taskinstance import TaskInstance
+from tests.test_utils.config import conf_vars
+
 
 DEFAULT_DATE = '2019-10-03'
 TEST_DAG_ID = 'test_my_custom_operator'
 
 class B2ShareOperatorTest(unittest.TestCase):
    def setUp(self):
-       self.dag = DAG(TEST_DAG_ID, schedule_interval='@daily', default_args={'start_date' : days_ago(2)})
+       self.dag = DAG(TEST_DAG_ID, schedule_interval='@daily', default_args={'start_date' : days_ago(2)}, params={"oid": "111"})
        self.op = B2ShareOperator(
            dag=self.dag,
            task_id='test',
@@ -19,8 +21,8 @@ class B2ShareOperatorTest(unittest.TestCase):
        self.ti = TaskInstance(task=self.op, execution_date=days_ago(1))
 
    def test_execute_no_trigger(self):
-       
-       self.ti.run(ignore_ti_state=False, context={'params': {'oid': 111}})
+       #with conf_vars({('params': {'oid': 1111})}):
+       self.ti.run(ignore_ti_state=False)
        print(self.ti.state)
        self.assertEqual(State.SUCCESS, self.ti.state)
        # Assert something related to tasks results
